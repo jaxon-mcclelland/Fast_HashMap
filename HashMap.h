12245682798aa32bed9dcbde92e5e2bf20a5b3d9
Jaxon McClelland <jmcclelland1844@protonmail.com>
2022-11-28 14:55:29 -0800

    Fixed some bugs, Improved hash function. More performance improvements needed

--- HashMap.h
+++ HashMap.h
@@ -1,8 +1,7 @@
 #ifndef __HASHMAP
 #define __HASHMAP
 
-#define mult_const 0.625292
-#define load_factor 0.75
+#define DEFAULT_LOAD_FACTOR 0.90
 
 #include <vector>
 
@@ -10,14 +9,14 @@ using std::pair;
 using std::vector;
 
 
-
+// power of two hash table
 template <class K, class V>
 class HashMap {
     public:
-        HashMap(int tableSize = 20);
+        HashMap();
         ~HashMap();
         void clear();
-        bool insert(std::pair<K,V> element, bool rehash_if_collision = true);
+        bool insert(const std::pair<K,V>& element);
         bool erase(const K& key);
         V operator[](const K& key) const;
 
@@ -27,26 +26,31 @@ class HashMap {
 
     private:
         // returns the bucket index of an element
-        int hash(const K& key) const;
+        unsigned int hash(const K& key) const;
         
         // rehashes the entire table
         void rehash();
 
-        int max_capacity = 0;
-        int num_items = 0;
-        vector<pair<K,V>> base_table;
-        
+        // find a slot in the hash table with a lower psl value than the new key
+        bool find_slot(const std::pair<K,V>& element, int psl, unsigned int idx);
 
+        // shift keys up from idx after an item has been deleted where idx represents an empty slot to be filled
+        void shift_keys(unsigned int deleted_idx);
 
+        unsigned int max_capacity = 0;
+        unsigned int num_items = 0;
+        unsigned int hash_const_a = 5;
+        unsigned int hash_const_b = 9;
+        vector<pair<pair<K,V>,int>> base_table;
 };
 
 
 
 template <class K, class V>
-HashMap<K,V>::HashMap(int tableSize = 20) {
-    max_capacity = tableSize;
+HashMap<K,V>::HashMap() {
+    max_capacity = 16;
     num_items = 0;
-    base_table = vector<pair<K,V>>(max_capacity);
+    base_table = vector<pair<pair<K,V>, int>>(max_capacity, {{-1, -1}, -1});
 }
 
 template <class K, class V>
@@ -59,21 +63,50 @@ HashMap<K,V>::~HashMap() {
 template <class K, class V>
 void HashMap<K,V>::clear() {
     base_table.clear();
+    this->base_table.resize(16);
+    this->max_capacity = 16;
 }
 
 template <class K, class V>
-bool HashMap<K,V>::insert(std::pair<K,V> element, bool rehash_if_collision = true) {
+bool HashMap<K,V>::insert(const std::pair<K,V>& element) {
+    if(get_load_factor() > DEFAULT_LOAD_FACTOR) {
+        rehash();
+    }
     int idx = hash(element.first);
-
+    if(base_table[idx].second == -1) {
+        base_table[idx] = {element, idx};
+    } else if(!find_slot(element, 1, ++idx)) {
+            return false;
+    }
+    ++num_items;
+    return true;
 }
 
 template <class K, class V>
 bool HashMap<K,V>::erase(const K& key) {
-    return false;
+    unsigned int idx = hash(key);
+    for(; idx < this->max_capacity; ++idx) {
+        if(base_table[idx].first.first == key) {
+            shift_keys(idx);
+            --num_items;
+            return true;
+        }
+        if(base_table[idx].second < 1) {
+            return false;
+        }
+    }
+    return true;
 }
 
 template <class K, class V>
 V HashMap<K,V>::operator[](const K& key) const {
+    unsigned int idx = hash(key);
+    while(idx < this->max_capacity && base_table[idx].second > 0) {
+        if(base_table[idx].first.first == key) {
+            return base_table[idx].first.second;
+        }
+        ++idx;
+    }
     return {};
 }
 
@@ -97,14 +130,55 @@ int HashMap<K,V>::get_num_items() const {
 /// *** PRIVATE METHODS *** ///
 
 template <class K, class V>
-int HashMap<K,V>::hash(const K& key) const {
-    return max_capacity * ((mult_const * key) % 1);
+unsigned int HashMap<K,V>::hash(const K& key) const {
+        
+    unsigned int hash_code = ((hash_const_a * key + hash_const_b));
+
+    int hash_val = hash_code & ((hash_code << 2) | (hash_code << 11));
+    return hash_val & (this->max_capacity - 1);    
+    
 }
 
 template <class K, class V>
 void HashMap<K,V>::rehash() {
-    return;
+    auto new_table = base_table;
+    base_table.clear();
+    this->max_capacity = this->max_capacity << 1;
+    base_table.resize(this->max_capacity, {{-1, -1}, -1});
+    hash_const_a = (hash_const_a << 1) + 1;
+    hash_const_b = (hash_const_b << 1) + 1;
+    for(auto itr:new_table) {
+        if(itr.second != -1) {
+            insert(itr.first);
+        } 
+    }
 }
+
+template <class K, class V>
+bool HashMap<K,V>::find_slot(const std::pair<K,V>& element, int psl, unsigned int idx) {
+    for(; idx < this->max_capacity; ++idx) {
+        if(base_table[idx].second == -1) {
+            base_table[idx] = {element, psl};
+            return true;
+        } else if(base_table[idx].second < psl) {
+            std::pair<K,V> original_occupant = base_table[idx].first;
+            int original_psl = base_table[idx].second;
+            base_table[idx] = {element, psl};
+            find_slot(original_occupant, ++original_psl, ++idx);
+            return true;
+        }
+        ++psl;
+    }
+    return false;
+}
+
+template <class K, class V>
+void HashMap<K,V>::shift_keys(unsigned int deleted_idx) {
+    while(deleted_idx < this->max_capacity - 1 && base_table[deleted_idx].second > 0) {
+        base_table[deleted_idx] = base_table[++deleted_idx];
+    }
+}
+
 
 
 #endif
